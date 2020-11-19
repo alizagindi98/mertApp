0769a0b689cdec6c876a3627d70e150ec58f7ae7
chaimjfishman <chaim.fishman2@gmail.com>
2020-11-19 00:13:01 -0500

    update to use Expo Linking

--- src/screens/FormsScreen/FormsScreen.js
+++ src/screens/FormsScreen/FormsScreen.js
@@ -1,32 +1,23 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
+import styles from './styles';
+import * as Linking from 'expo-linking';
 
 export default function FormsScreen(props) {
     const onPAProtocolPress = () => {
-        (e) => {
-            e.preventDefault();
-            window.location.href='https://www.health.pa.gov/topics/Documents/EMS/2020%20PA%20BLS%20Protocols.pdf';
-            }
+        Linking.openURL('https://www.health.pa.gov/topics/Documents/EMS/2020%20PA%20BLS%20Protocols.pdf');
     }
 
     const onProbationaryLogPress = () => {
-        (e) => {
-            e.preventDefault();
-            window.location.href='https://docs.google.com/spreadsheets/d/1JwWX54awiqbkUmAZoGOjP5DgkO30EMyK7vPrUyH9frU/edit#gid=0';
-            }
+        Linking.openURL('https://docs.google.com/spreadsheets/d/1JwWX54awiqbkUmAZoGOjP5DgkO30EMyK7vPrUyH9frU/edit#gid=0');
     }
 
     const onMertEquipmentPress = () => {
-        (e) => {
-            e.preventDefault();
-            window.location.href='https://docs.google.com/forms/d/e/1FAIpQLSeCPymiaEl2AgXcFctGIuE9jurxku6evYMS5RO1w1ywrPU5-w/viewform';
-            }
+        Linking.openURL('https://docs.google.com/forms/d/e/1FAIpQLSeCPymiaEl2AgXcFctGIuE9jurxku6evYMS5RO1w1ywrPU5-w/viewform');
     }
+    
     const onAcceptedAbbreviationsPress = () => {
-        (e) => {
-            e.preventDefault();
-            window.location.href='https://docs.google.com/spreadsheets/d/163IBjttNdGNvjn3-k9jIHE82uoupQFl4FG_XhJOCtBg/edit?ts=5b718fe6#gid=0';
-            }
+        Linking.openURL('https://docs.google.com/spreadsheets/d/163IBjttNdGNvjn3-k9jIHE82uoupQFl4FG_XhJOCtBg/edit?ts=5b718fe6#gid=0');
     }
 
     
