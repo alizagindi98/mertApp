12d55a5485b5c4de8bf7c845b84997c13762f4f5
Leah Voytovich <leahvoyt@gmail.com>
2020-12-20 17:09:21 -0500

    added initial schedule links

--- src/screens/ScheduleScreen/ScheduleScreen.js
+++ src/screens/ScheduleScreen/ScheduleScreen.js
@@ -1,20 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { Text, View, SafeAreaView, Image } from 'react-native';
 import styles from './styles';
-import { firebase } from '../../firebase/config'
+import FormLink from '../../components/FormsScreen/FormLink'
+// import { firebase } from '../../firebase/config'
 
 
 export default function ScheduleScreen(props) {
 
     return (
-        <SafeAreaView style={styles.verticalContainer}>
+        <SafeAreaView style={styles.schedContainer}>
 
-            <SafeAreaView style={styles.horizontalContainer}>
-                <SafeAreaView style={styles.verticalContainer}>
-                    <Text> Schedule Here </Text>
-                </SafeAreaView>
-            </SafeAreaView>
-            {/* Insert extra profile info here */}
+            <FormLink
+                title="Schedule"
+                link='https://docs.google.com/spreadsheets/d/1Pq4hw8gndR5udZWyPe-OUQprV2KWUIl4srjstPDu1AU/edit?usp=sharing'
+            />
+            <FormLink
+                title="Schedule Form"
+                link='https://media1.giphy.com/media/3o72FkiKGMGauydfyg/giphy.gif'
+            />
         </SafeAreaView>
     )
 }
---
+++ src/screens/ScheduleScreen/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+    schedContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: "flex-start",
+        alignItems: "Center",
+    },
+})
